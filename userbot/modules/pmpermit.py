a12f85fe18fabdabf54607b989bd30dd2ee6f83f
prono69 <55076817+prono69@users.noreply.github.com>
2019-09-10 11:36:03 +0530

    Update pmpermit.py

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -20,7 +20,7 @@ from userbot.events import register, errors_handler
 UNAPPROVED_MSG = (
     "`Bleep Blop! This is a Bot. Don't Freak xD.\n\n`"
     "`My Master hasn't approved you to PM yet.`"
-    "`Please wait for my Master to look in, He mostly approves PMs.\n\n`"
+    "`Please wait for my Great Master to look in, He mostly approves PMs.\n\n`"
     "`Until then, please don't spam my King's PM, you'll get blocked and reported!`")
 # =================================================================
 
